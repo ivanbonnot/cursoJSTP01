7a5ad5fde93c22b3c0e3f1f8822d691660d1e2dc
Ivan Bonnot <ivanbonnot@gmail.com>
2022-07-11 18:35:51 -0300

    coomit casa

---
+++ script.js
@@ -0,0 +1,11 @@
+let enter = parseInt(prompt("Enter a number"));
+
+for (let index = 0; index <= enter; index++){
+    console.log(index);
+
+    if (index == 10) {
+        alert(index);
+        break;
+    }    
+}
+
