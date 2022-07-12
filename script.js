de431b8b095b294a563e1a4d67ac5866a03f3a5d
Ivan Bonnot <ivanbonnot@gmail.com>
2022-07-11 23:35:50 -0300

    adv 01

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-let enter = parseInt(prompt("Enter a number"));
+// let enter = parseInt(prompt("Enter a number"));
 
-for (let index = 0; index <= enter; index++){
-    console.log(index);
+// for (let index = 0; index <= enter; index++){
+//     console.log(index);
 
-    if (index == 10) {
-        alert(index);
-        break;
-    }    
-}
+//     if (index == 10) {
+//         alert(index);
+//         break;
+//     }    
+// }
 
